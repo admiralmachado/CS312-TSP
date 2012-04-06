e96e703561874bd64e75a1322e91740458f4c95c
Spencer Machado <admiralmachado@gmail.com>
2012-04-06 13:29:25 -0600

    Initializing Project

---
+++ TSP/BoundedBrancher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSP
+{
+    class BoundedBrancher
+    {
+        private double bssf_cost;
+        private ArrayList bssf;
+
+    }
+}
