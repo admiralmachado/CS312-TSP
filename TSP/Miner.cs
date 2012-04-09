43ea0709aa5110528290eea4344353f8dd2a7284
Spencer Machado <admiralmachado@gmail.com>
2012-04-09 13:33:07 -0600

    Added necessary files grumble

---
+++ TSP/Miner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TSP
+{
+    class Miner
+    {
+        public int repeats { get; private set; }
+
+        public Miner()
+        {
+            repeats = 0;
+        }
+
+        public void Sing()
+        {
+            Console.WriteLine("[MINER] I will perform a classic piece from Snow White!");
+            while (true)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Hi Hoooo!");
+                ++repeats;
+            }
+        }
+    }
+}
